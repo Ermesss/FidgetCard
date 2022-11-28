4341ed2ba00c580af0b68e259ddd3bf011e7cf5f
Ermanno <ermannofissore@gmail.com>
2022-11-28 09:40:41 +0000

    Initial Commit

---
+++ FidgetCard/FidgetCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidgetCardApp.swift
+//  FidgetCard
+//
+//  Created by Ermanno Fissore on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FidgetCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
